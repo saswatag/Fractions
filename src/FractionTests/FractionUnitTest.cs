84a6dd1dde29740309515093e27bd04ab2dacca7
Sas <saswata66@gmail.com>
2021-07-18 14:32:48 +1000

    Fractions in their lowest forms with same denominator
    test multiple combinations of fractions in their lowest forms with same denominator

--- src/FractionTests/FractionUnitTest.cs
+++ src/FractionTests/FractionUnitTest.cs
@@ -33,15 +33,18 @@ namespace FractionTests
             createFractionAction.Should().Throw<ArgumentException>().WithMessage("Fraction with denominator zero is invalid.");
         }
 
-        [Fact]
-        public void Fractions_In_Lowest_Form_With_Same_Denomitor_Are_Summed_correctly()
+        [Theory]
+        [MemberData(nameof(FractionsInLowestFromWithSameDenominator))]
+        public void Fractions_In_Lowest_Form_With_Same_Denomitor_Are_Summed_correctly(int numerator1, int denominator1, int numerator2, int denominator2, int expectedNumerator,
+            int expectedDenominator)
         {
-            var operand1 = new Fraction(1, 5);
-            var operand2 = new Fraction(3, 5);
+            var operand1 = new Fraction(numerator1, denominator1);
+            var operand2 = new Fraction(numerator2, denominator2);
 
             Fraction sumResult = operand1.Add(operand2);
 
-            Assert.True(sumResult.Numerator.Equals(4) && sumResult.Denominator.Equals(5));
+            Assert.True(sumResult.Numerator.Equals(expectedNumerator) && sumResult.Denominator.Equals(expectedDenominator),
+                $"Expected numerator: {expectedNumerator} and denominator: {expectedDenominator}, but got numerator: {sumResult.Numerator} and denominator: {sumResult.Denominator}");
         }
 
         #region Heplers
@@ -55,6 +58,19 @@ namespace FractionTests
                 new object[] { 1, 1, 2, 1, 3, 1 },
                 new object[] { 0, 1, 1, 1, 1, 1 },
                 new object[] { 10000, 1, 1, 1, 10001, 1 }
+            };
+        }
+
+        public static IEnumerable<object[]> FractionsInLowestFromWithSameDenominator()
+        {
+            return new List<object[]>
+            {
+                // numerator1, denominator1, numerator2, denominator2, expectedNumerator, expectedDenominator
+                new object[] { 1, 1, 1, 1, 2, 1 },
+                new object[] { 1, 2, 3, 2, 4, 2 },
+                new object[] { 0, 1, 1, 1, 1, 1 },
+                new object[] { 5, 1, 5, 1, 10, 1 },
+                new object[] { 2, 3, 1, 3, 3, 3 }
             };
         }
 
