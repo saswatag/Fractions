3ccdeb4ac0820fc0f196228e467157590058603f
Sas <saswata66@gmail.com>
2021-07-18 14:36:44 +1000

    Remove duplication
    Remove duplication between test for denominator 1 and factions in lowest form that have same denominators

--- src/FractionTests/FractionUnitTest.cs
+++ src/FractionTests/FractionUnitTest.cs
@@ -10,7 +10,8 @@ namespace FractionTests
     {
         [Theory]
         [MemberData(nameof(FractionsWithDenominator_1))]
-        public void Fractions_With_Denomitor_1_Are_Summed_Correctly(int numerator1, int denominator1, int numerator2, int denominator2, int expectedNumerator, 
+        [MemberData(nameof(FractionsInLowestFromWithSameDenominator))]
+        public void Fractions_With_Same_Denomitor_Are_Summed_Correctly(int numerator1, int denominator1, int numerator2, int denominator2, int expectedNumerator, 
             int expectedDenominator)
         {
             var operand1 = new Fraction(numerator1, denominator1);
@@ -31,20 +32,6 @@ namespace FractionTests
             Action createFractionAction = () => new Fraction(numerator, deominator);
 
             createFractionAction.Should().Throw<ArgumentException>().WithMessage("Fraction with denominator zero is invalid.");
-        }
-
-        [Theory]
-        [MemberData(nameof(FractionsInLowestFromWithSameDenominator))]
-        public void Fractions_In_Lowest_Form_With_Same_Denomitor_Are_Summed_correctly(int numerator1, int denominator1, int numerator2, int denominator2, int expectedNumerator,
-            int expectedDenominator)
-        {
-            var operand1 = new Fraction(numerator1, denominator1);
-            var operand2 = new Fraction(numerator2, denominator2);
-
-            Fraction sumResult = operand1.Add(operand2);
-
-            Assert.True(sumResult.Numerator.Equals(expectedNumerator) && sumResult.Denominator.Equals(expectedDenominator),
-                $"Expected numerator: {expectedNumerator} and denominator: {expectedDenominator}, but got numerator: {sumResult.Numerator} and denominator: {sumResult.Denominator}");
         }
 
         #region Heplers
