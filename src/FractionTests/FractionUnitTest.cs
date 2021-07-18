c19623fe94092021f6af30c5f35f54251fff1d80
Sas <saswata66@gmail.com>
2021-07-18 14:13:22 +1000

    Use xunit theory to test combinations
    Use xunit Theory to test multiple combinations of fractions with denominator 1

--- src/FractionTests/FractionUnitTest.cs
+++ src/FractionTests/FractionUnitTest.cs
@@ -1,5 +1,6 @@
 using FluentAssertions;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using Xunit;
 
@@ -7,15 +8,18 @@ namespace FractionTests
 {
     public class FractionUnitTest
     {
-        [Fact]
-        public void Fractions_With_Denomitor_1_Are_Summed_correctly()
+        [Theory]
+        [MemberData(nameof(FractionsWithDenominator_1))]
+        public void Fractions_With_Denomitor_1_Are_Summed_Correctly(int numerator1, int denominator1, int numerator2, int denominator2, int expectedNumerator, 
+            int expectedDenominator)
         {
-            var operand1 = new Fraction(2, 1);
-            var operand2 = new Fraction(3, 1);
+            var operand1 = new Fraction(numerator1, denominator1);
+            var operand2 = new Fraction(numerator2, denominator2);
 
             Fraction sumResult = operand1.Add(operand2);
-            
-            Assert.True(sumResult.Numerator.Equals(5) && sumResult.Denominator.Equals(1));
+
+            Assert.True(sumResult.Numerator.Equals(expectedNumerator) && sumResult.Denominator.Equals(expectedDenominator), 
+                $"Expected numerator: {expectedNumerator} and denominator: {expectedDenominator}, but got numerator: {sumResult.Numerator} and denominator: {sumResult.Denominator}");
         }
 
         [Theory]
@@ -28,5 +32,32 @@ namespace FractionTests
 
             createFractionAction.Should().Throw<ArgumentException>().WithMessage("Fraction with denominator zero is invalid.");
         }
+
+        [Fact]
+        public void Fractions_In_Lowest_Form_With_Same_Denomitor_Are_Summed_correctly()
+        {
+            var operand1 = new Fraction(1, 5);
+            var operand2 = new Fraction(3, 5);
+
+            Fraction sumResult = operand1.Add(operand2);
+
+            Assert.True(sumResult.Numerator.Equals(4) && sumResult.Denominator.Equals(5));
+        }
+
+        #region Heplers
+
+        public static IEnumerable<object[]> FractionsWithDenominator_1()
+        {
+            return new List<object[]>
+            {
+                // numerator1, denominator1, numerator2, denominator2, expectedNumerator, expectedDenominator
+                new object[] { 2, 1, 3, 1, 5, 1 },
+                new object[] { 1, 1, 2, 1, 3, 1 },
+                new object[] { 0, 1, 1, 1, 1, 1 },
+                new object[] { 10000, 1, 1, 1, 10001, 1 }
+            };
+        }
+
+        #endregion
     }
 }
