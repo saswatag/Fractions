18a4d2ae246cfb4be1a421aac69617e7ce802687
Sas <saswata66@gmail.com>
2021-08-14 20:20:57 +1000

    Communicate the intent that the test data selected can be any random fraction

--- src/FractionTests/ReduceFractionTests.cs
+++ src/FractionTests/ReduceFractionTests.cs
@@ -7,6 +7,7 @@ using Xunit;
 using FluentAssertions;
 using FluentAssertions.Common;
 using FractionOperation;
+using System.Reflection.Metadata.Ecma335;
 
 namespace FractionTests
 {
@@ -15,15 +16,17 @@ namespace FractionTests
         [Fact]
         public void ReduceFractionNotInLowestTerms()
         {
-            Fraction reducedFraction = new GCDFractionReducer().Reduce(new Fraction(4, 8));
-            reducedFraction.Should().Be(new Fraction(1, 2));
+            Fraction reducedFraction = new GCDFractionReducer().Reduce(AnyUnReducedFractionAndTheCorrespondingReducedForm().UnReducedFraction);
+            reducedFraction.Should().Be(AnyUnReducedFractionAndTheCorrespondingReducedForm().ExpectedReducedFraction);
         }
 
         [Fact]
         public void ReduceFractionAreadyInLowestTerms()
         {
-            Fraction reducedFraction = new GCDFractionReducer().Reduce(new Fraction(1, 2));
-            reducedFraction.Should().Be(new Fraction(1, 2));
+            Fraction fractioInLowestTerms = AnyFractionInLowestTerms();
+
+            Fraction reducedFraction = new GCDFractionReducer().Reduce(fractioInLowestTerms);
+            reducedFraction.Should().Be(fractioInLowestTerms);
         }
 
         [Fact]
@@ -32,5 +35,14 @@ namespace FractionTests
             Fraction reducedFraction = new GCDFractionReducer().Reduce(new Fraction(0, 6));
             reducedFraction.Should().Be(new Fraction(0, 6));
         }
+
+        #region Helpers
+
+        private (Fraction UnReducedFraction, Fraction ExpectedReducedFraction) AnyUnReducedFractionAndTheCorrespondingReducedForm() => 
+            (UnReducedFraction: new Fraction(4, 8), ExpectedReducedFraction: new Fraction(1, 2));
+
+        private Fraction AnyFractionInLowestTerms() => new Fraction(3, 5);
+
+        #endregion
     }
 }
