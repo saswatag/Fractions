9e6dc8c6f4265942dc619f6610a625de2548a52d
Sas <saswata66@gmail.com>
2021-08-25 21:25:46 +1000

    Remove unwanted using statements

--- src/FractionTests/ReduceFractionContractTests.cs
+++ src/FractionTests/ReduceFractionContractTests.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit;
+﻿using Xunit;
 using FluentAssertions;
-using FluentAssertions.Common;
-using FractionOperation;
+
 using FractionOperations.Collaborators;
 
 namespace FractionTests
